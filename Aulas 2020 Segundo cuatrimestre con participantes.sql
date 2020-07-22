63585b8bbcbb0525aec152600bfc0ab328d16206
Lucas Frías <lucasmatiasfrias@live.com>
2020-07-22 20:20:52 -0300

    Update Aulas 2020 Segundo cuatrimestre con participantes.sql

--- Aulas 2020 Segundo cuatrimestre con participantes.sql
+++ Aulas 2020 Segundo cuatrimestre con participantes.sql
@@ -7,51 +7,31 @@ concat(
         '</a>'
     ) as "Categoría",
 concat(
-        '<a target="_new" href="%%WWWROOT%%/course/view.php?id=',
+        '<a target="_new" href="%%WWWROOT%%/course/edit.php?id=',
         c.id,
         '">',
         c.shortname,
         '</a>'
     ) as "Aula virtual",
+c.fullname,
+    c.summary,
+    c.visible,
     to_timestamp(c.startdate) as "Fecha de inicio",
     to_timestamp(c.enddate) as "Fecha de fin",
-    (
-        SELECT
-            COUNT(ue.id)
-        FROM
-            prefix_course AS course
-            JOIN prefix_enrol AS en ON en.courseid = course.id
-            JOIN prefix_user_enrolments AS ue ON ue.enrolid = en.id
-        where
-            course.id = c.id
-    ) as "Participantes inscritos",
-    (
-        SELECT
-            COUNT(distinct asg.userid) AS "Estudiantes"
-        FROM
-            prefix_role_assignments AS asg
-            JOIN prefix_context AS context ON asg.contextid = context.id
-            AND context.contextlevel = 50
-            JOIN prefix_course AS course ON context.instanceid = course.id
-            JOIN prefix_role as r ON asg.roleid = r.id
-        WHERE
-            r.shortname like 'student'
-            and course.id = c.id
-    ),
-    (
-        SELECT
-            COUNT(distinct asg.userid) AS "Profesores"
-        FROM
-            prefix_role_assignments AS asg
-            JOIN prefix_context AS context ON asg.contextid = context.id
-            AND context.contextlevel = 50
-            JOIN prefix_course AS course ON context.instanceid = course.id
-            JOIN prefix_role as r ON asg.roleid = r.id
-        WHERE
-            r.shortname like any(array ['teacher','editingteacher'])
-            and course.id = c.id
-    )
+    sum(CASE
+               WHEN r.shortname like any(array ['teacher','editingteacher']) THEN 1
+	       ELSE 0
+	      END) as "Profesores",
+     sum(CASE
+               WHEN r.shortname like 'student' THEN 1
+	       ELSE 0
+	      END) as "Estudiantes"
 FROM
-    prefix_course as c inner join prefix_course_categories as cat on cat.id=c.category
-where getCategoryPath(cat.id) like 'SUPERIOR/2020.2 - Grado y Pregrado%'
+prefix_course as c 
+inner join prefix_course_categories as cat on cat.id=c.category
+inner join prefix_context as context on context.contextlevel=50 and c.id=context.instanceid
+inner join prefix_role_assignments as asg on asg.contextid=context.id
+inner join prefix_role as r on r.id=asg.roleid
+where cat.parent=144
+group by c.id, cat.id
 order by "Categoría", "Aula virtual"
