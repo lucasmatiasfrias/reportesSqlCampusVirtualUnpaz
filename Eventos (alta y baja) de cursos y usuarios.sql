eec7620a4f53a43c3e7387a90ac8aa284f2cd1e4
Lucas Frías <lucasmatiasfrias@live.com>
2021-02-23 10:43:06 -0300

    Se optimizó la consulta y se agregó una funcion para ingresar un timestamp y obtener su correspondiente bigint

--- Eventos (alta y baja) de cursos y usuarios.sql
+++ Eventos (alta y baja) de cursos y usuarios.sql
@@ -1,15 +1,15 @@
 select
     l.eventname,
-    to_timestamp(l.timecreated)
+    to_timestamp(l.timecreated), l.timecreated
 from
     prefix_logstore_standard_log as l
 where
+l.timecreated > extract(EPOCH FROM TIMESTAMP WITH TIME ZONE '2020-11-29 00:00:00-3') and
     (
         l.eventname like '%course_created%'
         or l.eventname like '%user_created%'
         or l.eventname like '%course_deleted%'
         or l.eventname like '%user_deleted%'
     )
-    and l.timecreated > 1575046831
 order by
     l.timecreated
