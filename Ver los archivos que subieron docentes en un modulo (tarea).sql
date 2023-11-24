4192d942cb90c47da481ae269b78c11cefdd6b0f
Braian Coronel <48736315+Braianxp@users.noreply.github.com>
2023-11-24 14:23:43 -0300

    Update Ver los archivos que subieron docentes en un modulo (tarea).sql

--- Ver los archivos que subieron docentes en un modulo (tarea).sql
+++ Ver los archivos que subieron docentes en un modulo (tarea).sql
@@ -3,8 +3,12 @@ DISTINCT u.username as username,u.email,u.firstname,u.lastname,
 course.shortname as course1,
 r.shortname as role1,
 ue.status as enrolstatus1,
-f.*,cont.*,
-a.*
+f.filename as nombreDelArchivo,
+f.source as fuente,
+f.author as autor,
+a.name as nombreActividad,
+a.intro as descripcion,
+a.activity as instruccionesActividad
 
 FROM
 prefix_course as course inner join prefix_context as c on c.instanceid=course.id /*and c.contextlevel=50*/
@@ -16,10 +20,12 @@ inner join prefix_enrol as e on ue.enrolid=e.id and e.courseid = course.id
 inner join prefix_files as f on f.userid = u.id
 inner join prefix_context as cont on cont.id = f.contextid
 inner join prefix_course_modules as cm on cm.id = cont.instanceid
-inner join prefix_assign as a on a.id = cm.instance
+inner join prefix_assign as a on a.id = cm.instance and a.course = course.id
 WHERE 
-	course.id in (13444)and (/*(r.shortname = 'student') or */(r.shortname = 'editingteacher')) and ue.status = 0
+	course.id in (15920)and (/*(r.shortname = 'student') or */(r.shortname = 'editingteacher')) and ue.status = 0
 and	f.filesize > 0
 and f.filearea = 'introattachment'
-and a.name = 'Actividad Prueba1'
+/*and a.name = 'Actividad Prueba1'*/
 ORDER BY course1,enrolstatus1,role1,username
+
+
